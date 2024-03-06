a9cf37870893591848b21cd48915eae492db08db
Gardel Alejandro Otero Verdugo <angrygaru@gmail.com>
2024-03-05 17:24:35 -0700

    Javascript mejor

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCJDa8CZRHiL9fnPEyiqcL8FhztVk8WsZs",
+  authDomain: "tecapps2024.firebaseapp.com",
+  projectId: "tecapps2024",
+  storageBucket: "tecapps2024.appspot.com",
+  messagingSenderId: "875183279926",
+  appId: "1:875183279926:web:65223cce1be5062c282d97"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+
+const firestore = firebase.firestore();
